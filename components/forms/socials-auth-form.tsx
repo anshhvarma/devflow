1a40e0b1ddac08fe200910bb88933b1e9ea2629f
Ansh Varma <anshvarma83691@gmail.com>
2025-08-16 17:57:09 +0530

    Github Login Complete

--- components/forms/socials-auth-form.tsx
+++ components/forms/socials-auth-form.tsx
@@ -1,33 +1,57 @@
+"use client";
+
 import Image from "next/image";
+import { signIn } from "next-auth/react";
 import React from "react";
+import { toast } from "sonner";
+
+import ROUTES from "@/constant/route";
 
 import { Button } from "../ui/button";
-
 const SocialAuthForm = () => {
   const buttonClass =
-    "background-dark400_light900 body-medium text-dark200_light800 rounded-2 min-h-12 flex-1 px-4 py-3.5";
+    "background-dark400_light900 body-medium text-dark200_light800 min-h-12 flex-1 rounded-2 px-4 py-3.5";
+
+  const handleSignIn = async (provider: "github" | "google") => {
+    try {
+      await signIn(provider, {
+        callbackUrl: ROUTES.HOME,
+        // redirect: false,
+      });
+    } catch (error) {
+      console.log(error);
+
+      toast("Sign-in Failed", {
+        description:
+          error instanceof Error
+            ? error.message
+            : "An error occured during sign-in",
+      });
+    }
+  };
 
   return (
     <div className="mt-10 flex flex-wrap gap-2.5">
-      <Button className={buttonClass}>
+      <Button className={buttonClass} onClick={() => handleSignIn("github")}>
         <Image
-          src={"icons/github.svg"}
-          alt="GitHub Logo"
+          src="/icons/github.svg"
+          alt="Github Logo"
           width={20}
           height={20}
-          className="mr-2.5 invert-colors object-contain"
+          className="invert-colors mr-2.5 object-contain"
         />
-        <span> Login with Github</span>
+        <span>Log in with GitHub</span>
       </Button>
-      <Button className={buttonClass}>
+
+      <Button className={buttonClass} onClick={() => handleSignIn("google")}>
         <Image
-          src={"icons/google.svg"}
-          alt="GooletHub Logo"
+          src="/icons/google.svg"
+          alt="Google Logo"
           width={20}
           height={20}
           className="mr-2.5 object-contain"
         />
-        <span> Login with Google</span>
+        <span>Log in with Google</span>
       </Button>
     </div>
   );
