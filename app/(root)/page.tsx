efb43252973608d49cb0651f434b3b28b13f3c62
Ansh Varma <anshvarma83691@gmail.com>
2025-12-21 12:55:39 +0530

    local search bar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,17 +1,84 @@
+import Link from "next/link";
 import React from "react";
 
-import { auth } from "@/auth";
+import LocalSearch from "@/components/search/local-search";
+import { Button } from "@/components/ui/button";
+import ROUTES from "@/constant/route";
 
-const Home = async () => {
-  const session = await auth();
+const question = [
+  {
+    _id: "1",
+    title: "What is the best way to learn React?",
+    description: "What is the best way to learn React?",
+    tags: [
+      { _id: "1", name: "react" },
+      { _id: "2", name: "javascript" },
+      { _id: "3", name: "typescript" },
+    ],
+    author: {
+      _id: "1",
+      name: "John Doe",
+    },
+    upvotes: 10,
+    answers: 5,
+    views: 100,
+    createdAt: new Date(),
+  },
+  {
+    _id: "2",
+    title: "What is javascript?",
+    description: "What is the best way to learn React?",
+    tags: [
+      { _id: "1", name: "react" },
+      { _id: "2", name: "javascript" },
+      { _id: "3", name: "typescript" },
+    ],
+    author: {
+      _id: "1",
+      name: "John Doe",
+    },
+    upvotes: 10,
+    answers: 5,
+    views: 100,
+    createdAt: new Date(),
+  },
+];
 
-  console.log(session);
+interface SearchParams {
+  searchParams: Promise<{ [key: string]: string }>;
+}
+const Home = async ({ searchParams }: SearchParams) => {
+  const { query = "" } = await searchParams;
+
+  const FilterQuestion = question.filter((question) =>
+    question.title.toLowerCase().includes(query?.toLowerCase())
+  );
   return (
-    <div>
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Possimus
-      perferendis consectetur doloribus fugit vitae dolore ipsa similique totam
-      corrupti, velit corporis dolor ullam amet. Odit eum nemo non odio dolorem.
-    </div>
+    <>
+      <section className="flex w-full flex-col-reverse gap-4 justify-between sm:flex-row sm:items-center">
+        <h1 className="h1-bold text-dark100_light900"> All Questions</h1>
+        <Button
+          className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900"
+          asChild
+        >
+          <Link href={ROUTES.ASK_QUESTION}>Ask a Question ?</Link>
+        </Button>
+      </section>
+      <section className="mt-11">
+        <LocalSearch
+          route="/"
+          imgSrc="/icons/search.svg"
+          placeholder="Seach Questions..."
+          otherClasses="flex-1"
+        />
+      </section>
+      homeFilter
+      <div className="mt-10 flex w-full flex-col gap-5">
+        {FilterQuestion.map((question) => (
+          <h1 key={question._id}>{question.title} </h1>
+        ))}
+      </div>
+    </>
   );
 };
 
---
+++ components/cards/question-card.tsx
@@ -0,0 +1,21 @@
+// import React from "react";
+
+// interface Props {
+//   question: Question;
+// }
+
+// const QuestionCards = ({
+//   question: { id, title, tags, author, createdAt, upvotes, answer, view },
+// }: Props) => {
+//   return <div className="card-wrapper rounded-[20px] ">{_id}</div>;
+// };
+
+// export default QuestionCards;
+
+import React from "react";
+
+const QuestionCard = () => {
+  return <div>QuestionCard</div>;
+};
+
+export default QuestionCard;
